4a5d0c65fc5e85ef89e90c0dd75ccfe51480d204
sagar <sagar@torenzo.com>
2019-05-08 18:27:42 +0530

    w4

--- src/main/java/com/ghana/app/qa/testdata/ConstantVariable.java
+++ src/main/java/com/ghana/app/qa/testdata/ConstantVariable.java
@@ -16,7 +16,7 @@ public class ConstantVariable {
 	public static boolean selectedCheckBoxOnDocSingle;
 	
 	//Buckets title
-	public static String newApplication="";
+	public static String newApplication="New Application";
 	public static String getTextFromApproveButtonFromCN="";
 	public static String getTextFromApproveFromHCD="";
 	
